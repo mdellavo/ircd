7414e55f699f57614ffcddcbc5349641668b0e2c
Marc DellaVolpe <marc.dellavolpe@gmail.com>
2016-01-21 22:50:31 -0500

    ability to parse mask from string

--- ircd/irc.py
+++ ircd/irc.py
@@ -88,17 +88,26 @@ class Mask(object):
         self.nickname = nickname or "*"
         self.user = user or "*"
         self.host = host or "*"
-        self.pattern = re.compile(self.pattern(), re.IGNORECASE)
+        self.pattern = re.compile(self.build_pattern(), re.IGNORECASE)
 
     def __str__(self):
         return "{nickname}!{user}@{host}".format(nickname=self.nickname, user=self.user, host=self.host)
 
-    def pattern(self):
-        glob = lambda s: "(" + s.replace("*", ".*?") + ")"
+    def __repr__(self):
+        return "<{}({})>".format(self.__class__.__name__, str(self))
+
+    def build_pattern(self):
+        glob = lambda s: "(" + s.replace(".", "\.").replace("*", ".+?") + ")"
         return "{nickname}!{user}@{host}$".format(nickname=glob(self.nickname), user=glob(self.user), host=glob(self.host))
 
     def match(self, identity):
         return self.pattern.match(identity) is not None
+
+    @classmethod
+    def parse(cls, s):
+        pattern = re.compile("([\w\*]+?)!([\w\*]+?)@([\w\*\.\-]+?)$", re.IGNORECASE)
+        match = pattern.match(s)
+        return cls(*match.groups()) if match else None
 
 
 class Channel(object):
